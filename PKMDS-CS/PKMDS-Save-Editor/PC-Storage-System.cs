925a282b8e7ab9b14ae55fd197e67116fc7b9203
Michael Bond <codemonkey85@gmail.com>
2015-04-16 11:23:27 -0400

    Some cleanup / organization

--- PKMDS-CS/PKMDS-Save-Editor/PC-Storage-System.cs
+++ PKMDS-CS/PKMDS-Save-Editor/PC-Storage-System.cs
@@ -1,6 +1,7 @@
 ﻿#region Using
 
 using System;
+using System.Linq;
 using System.Windows.Forms;
 using PKMDS_CS;
 
@@ -10,12 +11,12 @@ namespace PKMDS_Save_Editor
 {
     public partial class PKMDS_Save_Editor : Form
     {
-        private BindingSource bs = new BindingSource();
-        private string charizard = @"..\..\..\files\pk6\006 - Charizard - 853622BD286F.pk6";
-        private string jynx = @"..\..\..\files\pk6\124 - Jynx - 8028D005DE59.pk6";
-        private string mewtwo = @"..\..\..\files\pk6\150 - Mewtwo - 9AA4BEBE0B35.pk6";
-        private XYSav sav;
-        private string xysavfile = @"..\..\..\files\sav\PokemonXYDecrypted.sav";
+        private const string charizard = @"..\..\..\files\pk6\006 - Charizard - 853622BD286F.pk6";
+        private const string jynx = @"..\..\..\files\pk6\124 - Jynx - 8028D005DE59.pk6";
+        private const string mewtwo = @"..\..\..\files\pk6\150 - Mewtwo - 9AA4BEBE0B35.pk6";
+        private const string xysavfile = @"..\..\..\files\sav\PokemonXYDecrypted.sav";
+        private readonly BindingSource _bs = new BindingSource();
+        private XYSav _sav;
 
         public PKMDS_Save_Editor()
         {
@@ -24,16 +25,18 @@ namespace PKMDS_Save_Editor
 
         private void PKMDS_Save_Editor_Load(object sender, EventArgs e)
         {
-            sav = StructUtils.RawDeserialize<XYSav>(xysavfile);
-            foreach (PCBox box in sav.PCStorageSystem.Boxes)
+            LoadSave(xysavfile);
+            _bs.DataSource = _sav.PCStorageSystem.Boxes[5].Pokemon;
+            dgData.DataSource = _bs;
+        }
+
+        private void LoadSave(string saveFileName)
+        {
+            _sav = StructUtils.RawDeserialize<XYSav>(saveFileName);
+            foreach (var pokemon in _sav.PCStorageSystem.Boxes.SelectMany(box => box.Pokemon))
             {
-                foreach (Pokemon pokemon in box.Pokemon)
-                {
-                    PokePRNG.DecryptPokemon(pokemon);
-                }
+                PokePRNG.DecryptPokemon(pokemon);
             }
-            bs.DataSource = sav.PCStorageSystem.Boxes[0].Pokemon;
-            dgData.DataSource = bs;
         }
     }
 }
