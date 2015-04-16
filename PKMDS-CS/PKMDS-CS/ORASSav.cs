f7590b6fbde526e6ed835ee12e2a6fb66a57df67
Michael Bond <codemonkey85@gmail.com>
2015-04-16 15:28:47 -0400

    Started ORAS save support

---
+++ PKMDS-CS/PKMDS-CS/ORASSav.cs
@@ -0,0 +1,22 @@
+﻿#region Using
+
+using System;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace PKMDS_CS
+{
+    [StructLayout(LayoutKind.Explicit, Size = 0x100000, Pack = 1, CharSet = CharSet.Unicode)]
+    [Serializable]
+    public class ORASSav
+    {
+        [FieldOffset(0x38400)] [MarshalAs(UnmanagedType.Struct)] private PCStorageSystem pcstoragesystem;
+
+        public PCStorageSystem PCStorageSystem
+        {
+            get { return pcstoragesystem; }
+            set { pcstoragesystem = value; }
+        }
+    }
+}
--- PKMDS-CS/PKMDS-Save-Editor/PC-Storage-System.cs
+++ PKMDS-CS/PKMDS-Save-Editor/PC-Storage-System.cs
@@ -15,9 +15,17 @@ namespace PKMDS_Save_Editor
         private const string jynx = @"..\..\..\files\pk6\124 - Jynx - 8028D005DE59.pk6";
         private const string mewtwo = @"..\..\..\files\pk6\150 - Mewtwo - 9AA4BEBE0B35.pk6";
         private const string xysavfile = @"..\..\..\files\sav\PokemonXYDecrypted.sav";
+        private const string main = @"..\..\..\files\secret\main.sav";
+        private const string main1 = @"..\..\..\files\secret\main1.sav";
+        private const string main25 = @"..\..\..\files\secret\main25.sav";
+        private const string main37 = @"..\..\..\files\secret\main37.sav";
+        private const string ramsav = @"..\..\..\files\secret\ramsav.bin";
+        private const string ramsav2 = @"..\..\..\files\secret\ramsav2.bin";
+        private const string ramsav_combined = @"..\..\..\files\secret\ramsav_combined.bin";
         private readonly BindingSource _boxesBindingSource = new BindingSource();
         private CurrencyManager _boxesCurrencyManager;
         private XYSav _sav;
+        //private ORASSav _sav;
 
         public PKMDS_Save_Editor()
         {
@@ -26,7 +34,8 @@ namespace PKMDS_Save_Editor
 
         private void PKMDS_Save_Editor_Load(object sender, EventArgs e)
         {
-            LoadSave(xysavfile);
+            //LoadSave(xysavfile);
+            LoadSave(ramsav_combined);
             _boxesCurrencyManager = _boxesBindingSource.CurrencyManager;
             _boxesBindingSource.DataSource = _sav.PCStorageSystem.Boxes;
             dgData.DataSource = _boxesBindingSource;
@@ -37,6 +46,7 @@ namespace PKMDS_Save_Editor
         private void LoadSave(string saveFileName)
         {
             _sav = StructUtils.RawDeserialize<XYSav>(saveFileName);
+            //_sav = StructUtils.RawDeserialize<ORASSav>(saveFileName);
             foreach (var pokemon in _sav.PCStorageSystem.Boxes.SelectMany(box => box.Pokemon))
             {
                 PokePRNG.DecryptPokemon(pokemon);
